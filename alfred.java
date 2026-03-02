05bc189197083a7afd8d9b54d67c49593b94924d
Alf <alfredjbphiri@gmail.com>
2026-03-02 17:24:42 +0200

    Printing variables

--- alfred.java
+++ alfred.java
@@ -4,7 +4,7 @@ public class alfred {
 
 
         // variable declaration
-        
+
         int age = 26;
         String firstName = "Alfred";
         String lastName = "Phiri";
@@ -12,5 +12,14 @@ public class alfred {
         boolean isStudent = true;
         char gender = 'M';
         
+        // print variables
+
+        System.out.println("\n\n MY PERSONAL DETAILS\n\n");
+
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Gender: " + gender);
     }
 }
