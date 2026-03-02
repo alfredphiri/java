22f01e5b23b41f1849d62fabf4c06561870378dc
Alf <alfredjbphiri@gmail.com>
2026-03-02 17:21:25 +0200

    feat: add variables of different datatypes

--- alfred.java
+++ alfred.java
@@ -1,5 +1,16 @@
 public class alfred {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
+
+
+        // variable declaration
+        
+        int age = 26;
+        String firstName = "Alfred";
+        String lastName = "Phiri";
+        double height = 1.75;
+        boolean isStudent = true;
+        char gender = 'M';
+        
     }
 }
