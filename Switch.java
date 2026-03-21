fe6fc78fc36af395bca86c25d82a79132ca962db
alfred <113861777+alfredphiri@users.noreply.github.com>
2026-03-21 10:14:34 +0200

    create a modern switch

---
+++ Switch.java
@@ -0,0 +1,32 @@
+public Switch {
+
+  public static void main(String[] arguements){
+
+      String day = 2;
+
+      switch(day){
+
+        case 1 -> {
+          System.out.print("its monday");
+        }
+
+          case 1 -> {
+          System.out.print("its tuesday");
+        }
+
+          case 1 -> {
+          System.out.print("its wedsday";
+        }
+
+          case 1 -> {
+          System.out.print("its thurday");
+        }
+
+          case 1 -> {
+          System.out.print("its friday");
+        }
+
+      }
+
+  }
+}
